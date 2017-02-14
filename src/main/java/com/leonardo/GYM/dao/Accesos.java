14db820666a17e8286d11965adfbfed29c95f31b
Mac <Mac@DESKTOP-HH528TG.infor.com>
2017-02-14 20:16:38 +0100

    Auto-Tipo implementado

--- src/main/java/com/leonardo/GYM/dao/Accesos.java
+++ src/main/java/com/leonardo/GYM/dao/Accesos.java
@@ -6,56 +6,57 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.text.DateFormat;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Accesos {
 
-    DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-
+    private DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+    
     public Accesos() {
     }
 
-    ResultSet rs;
-
-    public void insertarAcceso(int idCliente, int tipo) {
+    public void insertarAcceso(int idCliente, int tipo) throws ParseException {
         try {
             Class.forName("com.mysql.jdbc.Driver");
             Connection conexion = DriverManager.getConnection("jdbc:mysql://db4free.net:3307/gimnasio", "davinci", "dam2davinci");
             Statement sentencia = conexion.createStatement();
-            
-            int tipoConsulta;
+
             Date date = new Date();
-            System.out.println(dateFormat.format(date));
+            Date ultimaFecha;
 
             String sqlTipo = String.format("SELECT fechahora, tipo FROM Accesos "
                     + "WHERE id_cliente = %s ORDER BY id_acceso DESC LIMIT 1", idCliente);
             ResultSet resultTipo = sentencia.executeQuery(sqlTipo);
-            resultTipo.next();
-            tipoConsulta = resultTipo.getInt(2);
-            
-            if (tipoConsulta == 0) {
+
+            if (resultTipo.isBeforeFirst()) {
+                resultTipo.next();
+                ultimaFecha = dateFormat.parse(resultTipo.getString(1));
+                if (resultTipo.getInt(2) == 0) {
+                    tipo = 1;
+                } else {
+                    tipo = 0;
+                }
+            } else {
+                resultTipo.close();
                 tipo = 1;
-            } else {
-                tipo = 0;
-            }
-            System.out.println(tipoConsulta);
-            System.out.println(resultTipo.getDate(1));
-            
-            System.out.println((date.getTime() - resultTipo.getDate(1).getTime())/(1000));
-            if ((date.getTime() - resultTipo.getDate(1).getTime())<(1000*60)) {   
-                System.out.println("No ha pasado un minuto");
-            } else {
-                System.out.println("Ha pasado más de un minuto");
+                ultimaFecha = date;
             }
 
             String sql = String.format("INSERT INTO Accesos(tipo, fechahora, id_cliente) VALUES (%s, '%s', '%s')", tipo,
                     dateFormat.format(date), idCliente);
-            System.out.println(sql);
-            //sentencia.executeUpdate(sql);
+            //System.out.println(sql);
 
+            if (date.getTime() - ultimaFecha.getTime() < (10000) && 
+                    date.getTime() - ultimaFecha.getTime() > 0) {
+                System.out.println("Esperate 10 segundos para pasar la tarjetita");
+            } else {
+                sentencia.executeUpdate(sql);
+                System.out.println("ACCESO INTRODUCIDO");
+            }
             conexion.close();
-            //System.out.println("ACCESO INTRODUCIDO");
+
         } catch (ClassNotFoundException cn) {
             cn.printStackTrace();
         } catch (SQLException e) {
