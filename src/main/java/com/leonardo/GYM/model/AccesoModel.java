ba34bcd15b749684142a5a444aad3e8c59b93b61
Mac <Mac@DESKTOP-HH528TG.infor.com>
2017-02-15 17:59:15 +0100

    AccesoModel añadido

---
+++ src/main/java/com/leonardo/GYM/model/AccesoModel.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.leonardo.GYM.model;
+
+/**
+ *
+ * @author Mac
+ */
+public class AccesoModel {
+    private int idAcceso, idCliente;
+    private String tipo, fechaHora;
+    
+    public AccesoModel() {
+
+    }
+
+    public int getIdAcceso() {
+        return idAcceso;
+    }
+
+    public void setIdAcceso(int idAcceso) {
+        this.idAcceso = idAcceso;
+    }
+
+    public int getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(int idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getFechaHora() {
+        return fechaHora;
+    }
+
+    public void setFechaHora(String fechaHora) {
+        this.fechaHora = fechaHora;
+    }
+}
